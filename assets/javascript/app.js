53d799935452eeba62c504aa54b26021f4f4a438
Scott <smcanally11@yahoo.com>
2018-11-11 16:53:51 -0500

    project one

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -67,7 +67,7 @@ $(document).ready(function () {
         $("#startButton").hide();
         $("#userInput").children().show();
         $("#title").html(movieArray[counter].original_title + "  " + "<span class='badge badge-secondary' id='score'></span>");
-        $("#poster").attr("src", "http://image.tmdb.org/t/p/w185/" + postersArray[counter]);
+        $("#poster").attr("src", "http://image.tmdb.org/t/p/w185/" + postersArray[counter]).attr("class", "animated bounce");
         $("#score").text(totalScore);
         $("#userInput").val("");
 
